5a6215b62ffb1ec4c5195d06ddcce4244c004b3c
CHAFF-Depew-Shehan <smparmar@usc.edu>
2019-07-29 17:51:21 -0700

    deleted old folders from master

--- GAMESS_workflow_pkg_07_02/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#result=$(python3 testMain.py ../R47/ True)
-while true; do 
-results=`python3 testMain.py "../R47/" "True"`
-eval "$results"
-done
--- GAMESS_workflow_pkg_07_02/testMain.py
+++
@@ -1,4 +0,0 @@
-import main as mn
-import sys
-var = (mn.main(sys.argv[1],eval(sys.argv[2])))
-print(var)
